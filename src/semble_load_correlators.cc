38f19c71fbdbb45f2bf21ddec9c1fb1833ab1cf4
Christopher Thomas <C.E.Thomas@damtp.cam.ac.uk>
2014-09-28 12:35:13 +0100

    Minor modification to correlator loading code to be compatible with updated adat Hadron::KeyHadronNPartIrrep_t structure

--- src/semble_load_correlators.cc
+++ src/semble_load_correlators.cc
@@ -1269,8 +1269,9 @@ namespace SEMBLE
                     keys[count].npoint[1].irrep.mom         = momList[i_mom];
                     keys[count].npoint[1].irrep.creation_op = false;
                     keys[count].npoint[1].irrep.smearedP    = opsxml[j_snk].smearedP;
-                    keys[count].npoint[1].irrep.CGs         = opsxml[j_snk].CGs;
-                    keys[count].npoint[1].irrep.ops         = opsxml[j_snk].ops;
+                    keys[count].npoint[1].irrep.op          = opsxml[j_snk].op;
+                    //keys[count].npoint[1].irrep.CGs         = opsxml[j_snk].CGs;
+                    //keys[count].npoint[1].irrep.ops         = opsxml[j_snk].ops;
 
                     // The source op
                     keys[count].npoint[2].t_slice           = keyParams.source_tslice;
@@ -1279,8 +1280,9 @@ namespace SEMBLE
                     keys[count].npoint[2].irrep.mom         = momList[i_mom];
                     keys[count].npoint[2].irrep.creation_op = true;
                     keys[count].npoint[2].irrep.smearedP    = opsxml[j_src].smearedP;
-                    keys[count].npoint[2].irrep.CGs         = opsxml[j_src].CGs;
-                    keys[count].npoint[2].irrep.ops         = opsxml[j_src].ops;
+                    keys[count].npoint[2].irrep.op          = opsxml[j_src].op;
+                    //keys[count].npoint[2].irrep.CGs         = opsxml[j_src].CGs;
+                    //keys[count].npoint[2].irrep.ops         = opsxml[j_src].ops;
 
                     count++;
                   } // loop over i_mom
